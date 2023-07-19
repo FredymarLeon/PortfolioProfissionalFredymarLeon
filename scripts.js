07677c0967af45a106ac83de4f7cf9204cf42972
Fredymar Leon <fredymar.leon@gmail.com>
2023-07-19 12:17:20 -0300

    Modificação de conteudo projetos, iniciei com JavaScript: addEventListener. Validaçao de dados.

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form')
+const nome = document.getElementById('username')
+const email = document.getElementById('email')
+const assunto = document.getElementById('assunto')
+const mensage = document.getElementById('mensagem')
+
+form.addEventListener("Submit", (e) => {
+    e.preventDefault();
+})
