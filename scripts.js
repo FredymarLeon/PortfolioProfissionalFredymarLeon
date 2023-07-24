1a05e1651008e639d75bedff2ca5c78c14e6ae66
Fredymar Leon <fredymar.leon@gmail.com>
2023-07-23 21:16:26 -0300

    Iniciando Validação de formulario com Javascrip, mas ainda não deu certo

--- scripts.js
+++ scripts.js
@@ -1,9 +1,62 @@
-const form = document.getElementById('form')
-const nome = document.getElementById('username')
-const email = document.getElementById('email')
-const assunto = document.getElementById('assunto')
-const mensage = document.getElementById('mensagem')
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
 
-form.addEventListener("Submit", (e) => {
-    e.preventDefault();
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+
+  if (usernameValue === "") {
+    setErrorFor(username, "O nome de usuário é obrigatório.");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "O email é obrigatório.");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  const formControls = form.querySelectorAll(".form-control");
+
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success";
+  });
+
+  if (formIsValid) {
+    console.log("O formulário está 100% válido!");
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  // Adicionar el mensaje de error
+  small.innerText = message;
+
+  // Adicionar la clase de error
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Adicionar la classe de éxito
+  formControl.className = "form-control success";
+}
+  //const regex 
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
